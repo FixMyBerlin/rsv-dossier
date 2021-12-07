4101bde2da12f3e49dccf4e8915bdae2ddb83d64
Tobias <tobias@fixmycity.de>
2021-12-07 11:57:08 +0100

    Prettier: reformat all

--- src/pages/using-ssr.js
+++ src/pages/using-ssr.js
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from '../components/layout';
+import Seo from '../components/seo';
 
 const UsingSSR = ({ serverData }) => {
   return (
@@ -10,13 +10,13 @@ const UsingSSR = ({ serverData }) => {
       <Seo title="Using SSR" />
       <h1>SSR page</h1>
       <img
-        style={{ width: "300px" }}
+        style={{ width: '300px' }}
         alt="A random dog"
         src={serverData.message}
       />
       <p>Welcome to a server side rendered page with a random dog photo</p>
       <p>
-        To learn more, head over to our{" "}
+        To learn more, head over to our{' '}
         <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/server-side-rendering/">
           documentation about Server Side Rendering
         </a>
@@ -24,25 +24,25 @@ const UsingSSR = ({ serverData }) => {
       </p>
       <Link to="/">Go back to the homepage</Link>
     </Layout>
-  )
-}
+  );
+};
 
-export default UsingSSR
+export default UsingSSR;
 
 export async function getServerData() {
   try {
-    const res = await fetch(`https://dog.ceo/api/breeds/image/random`)
+    const res = await fetch(`https://dog.ceo/api/breeds/image/random`);
     if (!res.ok) {
-      throw new Error(`Response failed`)
+      throw new Error(`Response failed`);
     }
     return {
       props: await res.json(),
-    }
+    };
   } catch (error) {
     return {
       status: 500,
       headers: {},
       props: {},
-    }
+    };
   }
 }
--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from '../components/layout';
+import Seo from '../components/seo';
 
 const UsingDSG = () => (
   <Layout>
@@ -10,7 +10,7 @@ const UsingDSG = () => (
     <h1>Hello from a DSG Page</h1>
     <p>This page is not created until requested by a user.</p>
     <p>
-      To learn more, head over to our{" "}
+      To learn more, head over to our{' '}
       <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/deferred-static-generation/">
         documentation about Deferred Static Generation
       </a>
@@ -18,6 +18,6 @@ const UsingDSG = () => (
     </p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
-)
+);
 
-export default UsingDSG
+export default UsingDSG;
