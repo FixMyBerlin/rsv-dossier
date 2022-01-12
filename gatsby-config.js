fae6265443e39e65d765826688c030c66fb6faeb
Tobias <tobias@fixmycity.de>
2022-01-12 20:51:38 +0100

    TailwindCSS: Install and configure
    Following the Guide on https://tailwindcss.com/docs/guides/gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,7 @@
 module.exports = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [],
+
+  // TailwindCSS needs PostCSS, https://tailwindcss.com/docs/guides/gatsby
+  plugins: ['gatsby-plugin-postcss'],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable global-require */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+  ],
+};
