8e808aaeb73c8a477263924bd3013c0b28b013dc
Henri <henri@fixmycity.de>
2022-02-02 12:54:16 +0100

    Modularize SplitContent component & add one more to homepage

--- src/components/Layout/Section/SplitContent.tsx
+++ src/components/Layout/Section/SplitContent.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { MapIcon } from '@heroicons/react/outline';
 
-export const SplitContent = () => {
+const stats = [
+  { title: 'Radschnellwege in Planung', number: '102' },
+  { title: 'Förderumfang', number: '16 Mio. €' },
+  {
+    title: 'Planungskilometer',
+    number: '260km',
+  },
+];
+
+export const SplitContent = (props) => {
+  const { title, mainStory, imageUrl, enableStats } = props;
   return (
     <div className="relative bg-white">
       <div className="h-56 bg-emerald-300 sm:h-72 lg:absolute lg:left-0 lg:h-full lg:w-1/2">
         <img
           className="h-full w-full object-cover"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/RS1_Radschnellweg_Ruhr_in_M%C3%BClheim_2.jpg/800px-RS1_Radschnellweg_Ruhr_in_M%C3%BClheim_2.jpg?uselang=de"
+          src={imageUrl}
           alt="Support team"
         />
       </div>
@@ -19,42 +29,25 @@ export const SplitContent = () => {
             </div>
           </div>
           <h2 className="mt-6 text-3xl font-extrabold text-gray-900 sm:text-4xl">
-            Deutschland wird Fahrradland
+            {title}
           </h2>
-          <p className="mt-6 text-lg text-gray-500">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore
-            nihil ea rerum ipsa. Nostrum consectetur sequi culpa doloribus
-            omnis, molestiae esse placeat, exercitationem magnam quod molestias
-            quia aspernatur deserunt voluptatibus.
-          </p>
-          <div className="mt-8 overflow-hidden">
-            <dl className="-mx-8 -mt-8 flex flex-wrap">
-              <div className="flex flex-col px-8 pt-8">
-                <dt className="order-2 text-base font-medium text-gray-500">
-                  Radschnellwege in Planung
-                </dt>
-                <dd className="order-1 text-2xl font-extrabold text-emerald-500 sm:text-3xl">
-                  102
-                </dd>
-              </div>
-              <div className="flex flex-col px-8 pt-8">
-                <dt className="order-2 text-base font-medium text-gray-500">
-                  Förderumfang
-                </dt>
-                <dd className="order-1 text-2xl font-extrabold text-emerald-500 sm:text-3xl">
-                  16 Mio. €
-                </dd>
-              </div>
-              <div className="flex flex-col px-8 pt-8">
-                <dt className="order-2 text-base font-medium text-gray-500">
-                  Planungskilometer
-                </dt>
-                <dd className="order-1 text-2xl font-extrabold text-emerald-500 sm:text-3xl">
-                  120km
-                </dd>
-              </div>
-            </dl>
-          </div>
+          <p className="mt-6 text-lg text-gray-500">{mainStory}</p>
+          {enableStats && (
+            <div className="mt-8 overflow-hidden">
+              <dl className="-mx-8 -mt-8 flex flex-wrap">
+                {stats.map((stat) => (
+                  <div className="flex flex-col px-8 pt-8">
+                    <dt className="order-2 text-base font-medium text-gray-500">
+                      {stat.title}
+                    </dt>
+                    <dd className="order-1 text-2xl font-extrabold text-emerald-500 sm:text-3xl">
+                      {stat.number}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,30 @@ const IndexPage: React.FC<PageProps> = ({ location }) => {
           </ButtonLink>
         </div>
       </Hero>
-      <SplitContent />
+      <div className="p-5">
+        <SplitContent
+          imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/RS1_Radschnellweg_Ruhr_in_M%C3%BClheim_2.jpg/800px-RS1_Radschnellweg_Ruhr_in_M%C3%BClheim_2.jpg?uselang=de"
+          mainStory="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore nihil ea rerum ipsa. Nostrum consectetur sequi culpa doloribus omnis, molestiae esse placeat, exercitationem magnam quod molestias quia aspernatur deserunt voluptatibus."
+          title="Deutschland wird Fahrradland"
+          enableStats
+        />
+      </div>
+      <div className="p-5">
+        <SplitContent
+          imageUrl="https://images.unsplash.com/photo-1541687664971-639c2f8b63f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          mainStory="Als Radschnellwege werden in Baden-Württemberg Radschnellverbindungen bezeichnet und wie folgt definiert:[1]
+
+          Gesamtstrecke mindestens fünf Kilometer
+          Potenzial von mindestens 2000 Fahrradfahrten täglich (im Durchschnitt außerorts)
+          Breite mindestens zwischen drei Metern (Richtungsverkehr) und vier Metern (Zweirichtungsverkehr)
+          in der Regel von anderen Verkehrsmitteln getrennt
+          Routen möglichst kreuzungsfrei oder mit minimalen Wartezeiten an Kreuzungen
+          Hohe Belagqualität und eine möglichst geringe Steigung
+
+      2017 beschloss das baden-württembergische Verkehrsministerium, drei Radschnellverbindungen zu realisieren, bei denen das Land Bauherr sein und die Baulast übernehmen soll: Heidelberg-Mannheim, Heilbronn-Neckarsulm-Bad Wimpfen und Stuttgart-Esslingen-Plochingen. Für diese drei „Pilotstrecken“ wurde prioritär mit der Planung und dem Bau begonnen.[2][3] Laut einer Pressemitteilung des Ministeriums für Verkehr vom September 2018 wurden in einer landesweiten Potentialanalyse „32 Radschnellwege mit einer Gesamtlänge von 500 Kilometer im vordringlichen Bedarf identifiziert. Weitere 20 Strecken könnten sich durch lokalspezifische Faktoren als würdig für eine Radschnellverbindung erweisen.“ Insgesamt sollen bis 2025 zunächst zehn Radschnellwege in Baden-Württemberg realisiert werden.[4]"
+          title="Hintergrund"
+        />
+      </div>
     </Layout>
   );
 };
