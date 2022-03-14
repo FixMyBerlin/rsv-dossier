95bcb1e10b848bf036e75d8a694d449e6297bb4d
Henri <henri@fixmycity.de>
2022-03-14 11:33:35 +0100

    Temporariliy disable navigation links of projects and map
    We will not publish those pages in the first release

--- src/components/Layout/Navigation/navigationLinks.const.ts
+++ src/components/Layout/Navigation/navigationLinks.const.ts
@@ -1,8 +1,8 @@
 export type navigationMenuItemProps = { name: string; to: string };
 
 export const navigationLinks: navigationMenuItemProps[] = [
-  { name: 'Karte', to: '/karte/' },
-  { name: 'Projekte', to: '/radschnellwege/' },
+  // { name: 'Karte', to: '/karte/' },
+  // { name: 'Projekte', to: '/radschnellwege/' },
   {
     name: 'Förderübersicht',
     to: '/foerderungen/',
