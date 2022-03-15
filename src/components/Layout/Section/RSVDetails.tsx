6c64fe9c64526b3f4fe6ed7b4d937268efb2540c
Laurenz <laurenz.rasche@gmail.com>
2022-03-15 11:13:34 +0100

    add box-shadow

--- src/components/Layout/Section/RSVDetails.tsx
+++ src/components/Layout/Section/RSVDetails.tsx
@@ -25,7 +25,7 @@ export const RSVDetails = (props) => {
       <div className="h-56 rounded-br-lg bg-emerald-300 sm:h-72 lg:absolute lg:left-0 lg:h-full lg:w-1/2">
         {/* TODO: replace with actual map */}
         <img
-          className="h-full w-full rounded-br-lg object-cover"
+          className="h-full w-full rounded-br-lg object-cover shadow-xl"
           src={
             'https://images.unsplash.com/photo-1541687664971-639c2f8b63f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
           }
