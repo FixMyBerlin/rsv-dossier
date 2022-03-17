b7bcf100fc8a1be120691435e1a202c046464880
Henri <henri@fixmycity.de>
2022-03-17 08:56:46 +0100

    Improve breaks in main headline in Hero

--- src/components/Layout/Hero/Hero.tsx
+++ src/components/Layout/Hero/Hero.tsx
@@ -24,9 +24,9 @@ export const Hero: React.FC<Props> = ({
         <main className="bg-emerald-400 px-4">
           <div className="mx-auto max-w-7xl pt-16 text-center sm:pt-24">
             <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block break-words text-white xl:inline">
+              <div className="block break-words text-white xl:inline">
                 {highlightedTitle}
-              </span>{' '}
+              </div>{' '}
               <span className="block xl:inline">{title}</span>
             </h1>
             <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
