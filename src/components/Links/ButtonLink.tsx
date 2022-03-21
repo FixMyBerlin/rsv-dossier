ed09b65724576d500ced9fe07c595e32cda0947e
Henri <henri@fixmycity.de>
2022-03-21 10:28:42 +0100

    Add outline and size property to ButtonLink, improve CSS structure

--- src/components/Links/ButtonLink.tsx
+++ src/components/Links/ButtonLink.tsx
@@ -2,16 +2,32 @@ import classNames from 'classnames';
 import { Link } from 'gatsby';
 import React from 'react';
 
-type Props = { to: string; className?: string; newWindow?: boolean };
+type ButtonSizes = 'sm' | 'md' | 'lg';
+
+type Props = {
+  to: string;
+  className?: string;
+  newWindow?: boolean;
+  size?: ButtonSizes;
+  outline?: boolean;
+};
 
 // TODO: Refactor all those link components to share more logic
 export const ButtonLink: React.FC<Props> = ({
   to,
   className = '',
   newWindow = false,
+  outline = false,
+  size = 'md',
   children,
 }) => {
-  const styles = classNames(className, 'btn-brand');
+  const styles = classNames(
+    className,
+    { 'btn-brand-outline': outline },
+    { 'w-full px-8 py-3 md:text-lg': size === 'lg' },
+    { 'px-3 py-1 text-sm font-medium': size === 'sm' },
+    'btn-brand'
+  );
 
   if (newWindow) {
     return (
