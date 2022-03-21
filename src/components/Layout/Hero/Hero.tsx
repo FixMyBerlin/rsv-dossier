b80270f605a78eea3ca3ff175a7dae0fc0b52642
Henri <henri@fixmycity.de>
2022-03-21 14:51:49 +0100

    Fix wrong closing tag

--- src/components/Layout/Hero/Hero.tsx
+++ src/components/Layout/Hero/Hero.tsx
@@ -26,7 +26,7 @@ export const Hero: React.FC<Props> = ({
             <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
               <div className="block break-words text-white xl:inline">
                 {highlightedTitle}
-              </span>{' '}
+              </div>{' '}
               <span className="block xl:inline">{title}</span>
             </h1>
             <p className="relative z-10 mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
