988984337da7950a7598212dd5bb708c89d1af33
Henri <henri@fixmycity.de>
2022-03-29 16:24:57 +0200

    Add new hero with mobile version and changed media queries

--- src/components/Layout/Hero/Hero.tsx
+++ src/components/Layout/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import HeroGraphic from './hero_bg.svg';
+import HeroGraphic from './hero-bg.svg';
+import HeroGraphicMobile from './hero-bg-mobile.svg';
 import { Navigation } from '../Navigation/Navigation';
 
 type Props = {
@@ -23,13 +24,13 @@ export const Hero: React.FC<Props> = ({
         <Navigation location={location} />
         <main className="bg-emerald-400 px-4">
           <div className="mx-auto max-w-7xl pt-16 text-center sm:pt-24">
-            <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+            <h1 className="relative z-10 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
               <div className="block break-words text-white xl:inline">
                 {highlightedTitle}
               </div>{' '}
               <span className="block xl:inline">{title}</span>
             </h1>
-            <p className="relative z-10 mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+            <p className="relative z-10 mx-auto mt-3 max-w-md text-base text-gray-500 sm:max-w-md sm:text-lg md:mt-5 md:max-w-lg md:text-xl lg:max-w-2xl">
               {description}
             </p>
 
@@ -38,8 +39,17 @@ export const Hero: React.FC<Props> = ({
                 {children}
               </div>
             )}
-            <div className="relative z-0 md:-mt-[8%]">
-              <img src={HeroGraphic} alt="Hero Grafik" className="" />
+            <div className="relative z-0 -mt-[15%] sm:-mt-[20%]">
+              <img
+                src={HeroGraphic}
+                alt="Hero Grafik"
+                className="hidden sm:block"
+              />
+              <img
+                src={HeroGraphicMobile}
+                alt="Hero Grafik"
+                className="sm:hidden"
+              />
             </div>
           </div>
         </main>
