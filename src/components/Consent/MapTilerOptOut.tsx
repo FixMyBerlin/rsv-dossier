e31de0b2db8985cf35c7bf6fb2d0605d6f14fd11
Laurenz <laurenz.rasche@gmail.com>
2022-03-31 19:14:14 +0200

    move MapTiler Consent components into seperate directory

---
+++ src/components/Consent/MapTilerOptOut.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { ExternalLink, TextLink } from '~/components/Links';
+
+export const MapTilerOptOut = () => {
+  const [consent, setConsent] = useState(
+    new Date(localStorage.getItem('MAPTILER_CONSENT') || undefined) < new Date()
+  );
+  const revokeConsent = () => {
+    setConsent(false);
+    localStorage.setItem(
+      'MAPTILER_CONSENT',
+      new Date(undefined).toDateString()
+    );
+  };
+  return (
+    <div>
+      <div>
+        <p>
+          Zur darstellung unserer Karten benutzen wird MapTiler wodurch wir eine
+          Weitergabe von Nutzerinformationen an dritte nicht vermeiden können.
+          MapTiler speichert nur kurzfristig notwendige Daten zu Deiner IP
+          Adressse.
+        </p>
+      </div>
+      {consent && (
+        <div className="mt-5">
+          <p>
+            Du hast der{' '}
+            <ExternalLink href="https://www.maptiler.com/consent-policy/">
+              Datenschutzbestimmungen von MapTiler
+            </ExternalLink>{' '}
+            zugestimmt.
+          </p>
+          <button
+            className="btn-brand-outline px-8 py-3 shadow md:text-lg"
+            onClick={revokeConsent}
+          >
+            Zurückziehen
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
