b8f94ba610bac17a0e9504dbf6afaec8bb5be847
Laurenz <laurenz.rasche@gmail.com>
2022-04-05 12:02:07 +0200

    style MapTilerOptIn

--- src/components/Consent/MapTilerOptIn.tsx
+++ src/components/Consent/MapTilerOptIn.tsx
@@ -7,15 +7,12 @@ export const MapTilerOptIn = ({ setConsent }) => {
     localStorage.setItem('MAPTILER_CONSENT', new Date().toDateString());
   };
   return (
-    <div className="justify-center bg-white shadow sm:rounded-lg">
+    <div className="h-fit self-center bg-white shadow sm:rounded-lg">
       <div className="px-4 py-5 sm:p-6">
-        <h3 className="text-lg font-medium leading-6 text-gray-900">
-          Datenschutzbestimmung
-        </h3>
         <div className="mt-2 max-w-xl text-sm text-gray-500">
           <p>
             Zur Nutzung der Kartenfunktion stimme bitte den{' '}
-            <ExternalLink href="https://www.maptiler.com/consent-policy/">
+            <ExternalLink href="https://www.maptiler.com/privacy-policy/">
               Datenschutzbestimmungen von MapTiler
             </ExternalLink>{' '}
             zu. MapTiler speichert nur kurzfristig notwendige Daten zu Deiner IP
