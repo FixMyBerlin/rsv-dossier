16c6daa15b1cb155542bfdbb60ed51a234565ead
Laurenz <laurenz.rasche@gmail.com>
2022-04-05 12:01:48 +0200

    text formating

--- src/components/Consent/MapTilerOptOut.tsx
+++ src/components/Consent/MapTilerOptOut.tsx
@@ -16,9 +16,23 @@ export const MapTilerOptOut = () => {
     <div>
       <div>
         <p>
-          MapTiler ist ein Kartendienst. Seine Nutzung ist vollkommen freiwillig. Anbieter ist die MapTiler AG, Höfnerstrasse 98, Unterägeri, Zug 6314 in der Schweiz. Die Schweiz gehört weder der EU noch dem EWR an. Sie ist vielmehr ein sog. Drittstaat, in dem die DSGVO nicht gilt. Jedoch gibt es für die Schweiz einen Angemessenheitsbeschluss der EU Kommission nach Artikel 45 DSGVO, welcher der Schweiz im Allgemeinen ein angemessenes, der DSGVO entsprechendes Datenschutzniveau attestiert. Bitte lesen Sie vor einer Nutzung von MapTiler den Compliance-Hinweis von MapTiler unter https://www.maptiler.com/privacy-policy/ durch. Danach erhebt die MapTiler AG zur Nutzung der Kartenfunktion und aus Gründen der IT-Sicherheit Ihre IP- Adresse. Bitte nutzen Sie MapTiler nur, wenn Sie dem Tool trauen. Im Verhältnis zu uns erfolgt Ihre Nutzung von MapTiler völlig anonym. Wir fragen keine personenbezogenen oder personenbeziehbaren Daten ab. Sofern wir solche freiwillig erhalten, löschen wir diese unverzüglich und unwiederbringlich.
-          
-          Hier können Sie Ihre Einwilligung zurückziehen.
+          MapTiler ist ein Kartendienst. Seine Nutzung ist vollkommen
+          freiwillig. Anbieter ist die MapTiler AG, Höfnerstrasse 98,
+          Unterägeri, Zug 6314 in der Schweiz. Die Schweiz gehört weder der EU
+          noch dem EWR an. Sie ist vielmehr ein sog. Drittstaat, in dem die
+          DSGVO nicht gilt. Jedoch gibt es für die Schweiz einen
+          Angemessenheitsbeschluss der EU Kommission nach Artikel 45 DSGVO,
+          welcher der Schweiz im Allgemeinen ein angemessenes, der DSGVO
+          entsprechendes Datenschutzniveau attestiert. Bitte lesen Sie vor einer
+          Nutzung von MapTiler den Compliance-Hinweis von MapTiler unter
+          https://www.maptiler.com/privacy-policy/ durch. Danach erhebt die
+          MapTiler AG zur Nutzung der Kartenfunktion und aus Gründen der
+          IT-Sicherheit Ihre IP- Adresse. Bitte nutzen Sie MapTiler nur, wenn
+          Sie dem Tool trauen. Im Verhältnis zu uns erfolgt Ihre Nutzung von
+          MapTiler völlig anonym. Wir fragen keine personenbezogenen oder
+          personenbeziehbaren Daten ab. Sofern wir solche freiwillig erhalten,
+          löschen wir diese unverzüglich und unwiederbringlich. Hier können Sie
+          Ihre Einwilligung zurückziehen.
         </p>
       </div>
       {consent && (
@@ -28,14 +42,14 @@ export const MapTilerOptOut = () => {
             <ExternalLink href="https://www.maptiler.com/consent-policy/">
               Datenschutzbestimmungen von MapTiler
             </ExternalLink>{' '}
-           und dieser Datenschutzerklärung zugestimmt.
+            und dieser Datenschutzerklärung zugestimmt.
           </p>
           <button
             type="button"
             className="btn-brand-outline px-8 py-3 shadow md:text-lg"
             onClick={revokeConsent}
           >
-           Einwilligung Zurückziehen
+            Einwilligung Zurückziehen
           </button>
         </div>
       )}
