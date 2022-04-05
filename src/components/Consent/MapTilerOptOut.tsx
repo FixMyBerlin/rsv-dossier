964acf67a66600f9289edf2abd228e5beba35e91
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-04-05 10:44:54 +0200

    text edit
    Co-authored-by: Henri <henri@fixmycity.de>

--- src/components/Consent/MapTilerOptOut.tsx
+++ src/components/Consent/MapTilerOptOut.tsx
@@ -16,10 +16,9 @@ export const MapTilerOptOut = () => {
     <div>
       <div>
         <p>
-          Zur darstellung unserer Karten benutzen wird MapTiler wodurch wir eine
-          Weitergabe von Nutzerinformationen an dritte nicht vermeiden können.
-          MapTiler speichert nur kurzfristig notwendige Daten zu Deiner IP
-          Adressse.
+          MapTiler ist ein Kartendienst. Seine Nutzung ist vollkommen freiwillig. Anbieter ist die MapTiler AG, Höfnerstrasse 98, Unterägeri, Zug 6314 in der Schweiz. Die Schweiz gehört weder der EU noch dem EWR an. Sie ist vielmehr ein sog. Drittstaat, in dem die DSGVO nicht gilt. Jedoch gibt es für die Schweiz einen Angemessenheitsbeschluss der EU Kommission nach Artikel 45 DSGVO, welcher der Schweiz im Allgemeinen ein angemessenes, der DSGVO entsprechendes Datenschutzniveau attestiert. Bitte lesen Sie vor einer Nutzung von MapTiler den Compliance-Hinweis von MapTiler unter https://www.maptiler.com/privacy-policy/ durch. Danach erhebt die MapTiler AG zur Nutzung der Kartenfunktion und aus Gründen der IT-Sicherheit Ihre IP- Adresse. Bitte nutzen Sie MapTiler nur, wenn Sie dem Tool trauen. Im Verhältnis zu uns erfolgt Ihre Nutzung von MapTiler völlig anonym. Wir fragen keine personenbezogenen oder personenbeziehbaren Daten ab. Sofern wir solche freiwillig erhalten, löschen wir diese unverzüglich und unwiederbringlich.
+          
+          Hier können Sie Ihre Einwilligung zurückziehen.
         </p>
       </div>
       {consent && (
