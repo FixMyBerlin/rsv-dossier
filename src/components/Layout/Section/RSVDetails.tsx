b5a33fc59aff12f5d509242880012d947a7d3eb0
Laurenz <laurenz.rasche@gmail.com>
2022-04-13 14:36:04 +0200

    set `min-height`, `max-height` and  `max-width`

--- src/components/Layout/Section/RSVDetails.tsx
+++ src/components/Layout/Section/RSVDetails.tsx
@@ -27,8 +27,8 @@ type Props = {
 
 export const RSVDetails: React.VFC<Props> = ({ meta, geometry }) => {
   return (
-    <div className="relative bg-white">
-      <div className="flex h-80 overflow-hidden shadow-xl sm:h-80 lg:absolute lg:left-0 lg:h-full lg:w-1/2 lg:rounded-br-2xl">
+    <div className="relative max-h-[1080px] bg-white lg:min-h-[calc(100vh-theme(space.24)-1rem)]">
+      <div className="flex h-80 max-w-[960px] overflow-hidden shadow-xl lg:absolute lg:left-0 lg:h-full lg:w-1/2 lg:rounded-br-2xl">
         <RSVMap geometry={geometry} />
       </div>
       <div className=" mx-auto max-w-7xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16">
