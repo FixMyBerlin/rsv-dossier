d61b096875a6b3d430c9e726c2d19b59757fbaa8
Henri <henri@fixmycity.de>
2022-04-19 16:11:55 +0200

    Remove line-blur since it's not percentage

--- src/components/Map/RsvSegment.tsx
+++ src/components/Map/RsvSegment.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import { LineLayer, LineLayout } from 'mapbox-gl';
 import { Source, Layer } from 'react-map-gl';
-// import colors from 'tailwindcss/colors';
-
-// const stateColor = {
-//   idea: colors.emerald[900],
-//   agreed: colors.emerald[700],
-//   planning: colors.emerald[500],
-//   in_progress: colors.emerald[300],
-//   done: colors.emerald[100],
-// };
 
 const stateColor = {
   idea: '#A7F3D0',
@@ -26,20 +17,21 @@ type Props = {
   selected: number;
 };
 
-const layout: LineLayout = { 'line-cap': 'round', 'line-join': 'round' };
+const layout: LineLayout = {
+  'line-cap': 'round',
+  'line-join': 'round',
+};
 
 export const RSVSegment: React.FC<Props> = ({ feature, selected }) => {
   const { id } = feature.properties;
   const paint =
     selected === id
       ? {
-          'line-blur': 0.9,
           'line-color': selectedColor,
           'line-opacity': 1,
           'line-width': 7,
         }
       : {
-          'line-blur': 0.7,
           'line-color': stateColor[feature.properties.state],
           'line-opacity': 0.8,
           'line-width': 6,
