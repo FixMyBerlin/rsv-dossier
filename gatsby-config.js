7c7c8ce8365937154c91c04eeabe4a8846b0f7a3
Henri <henri@fixmycity.de>
2022-04-19 17:20:09 +0200

    Update gatsby sentry to 6.19.6

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,13 +14,11 @@ module.exports = {
     'gatsby-plugin-postcss',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
-    // '@sentry/gatsby',
     {
       resolve: '@sentry/gatsby',
       options: {
         dsn: 'https://5cc2dffb1c2c436fa3e34580af998942@o1174824.ingest.sentry.io/6272556',
         sampleRate: 0.0, // Adjust this value in production
-        // Cannot set `beforeSend`
       },
     },
     {
