007c02e2915628e35ed298cdb0178c92a1e1b8bb
Henri <henri@fixmycity.de>
2022-04-20 11:00:21 +0200

    Introduce environment variable files

--- sentry.config.js
+++ sentry.config.js
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/gatsby';
 
 Sentry.init({
-  dsn: 'https://5cc2dffb1c2c436fa3e34580af998942@o1174824.ingest.sentry.io/6272556',
+  dsn: process.env.GATSBY_SENTRY_DSN,
   sampleRate: 100,
 });
