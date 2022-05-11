100dff71ab2e8af8ca75911df169cc17f6afd1d4
Laurenz <laurenz.rasche@gmail.com>
2022-05-11 12:01:36 +0200

    use `node-fetch` instead of `axios`

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,7 +1,7 @@
 import { createFileNodeFromBuffer } from 'gatsby-source-filesystem';
-import axios from 'axios';
 import { staticMapRequest } from './src/staticmap';
-
+import fetch from 'node-fetch';
+import { Buffer } from 'buffer';
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 exports.onCreateWebpackConfig = ({ actions }) => {
@@ -32,11 +32,12 @@ exports.onCreateNode = async ({
   if (node.internal.type === 'GeometryJson') {
     const url = staticMapRequest(node, [800, 800]);
     // have to use createFileNodeFromBuffer due to url length limits in createRemoteFileNode :/
-    const response = await axios.get(url.toString(), {
-      responseType: 'arraybuffer',
-    });
+    const arrBuffer = await fetch(url.toString()).then((response) =>
+      response.arrayBuffer()
+    );
+
     createFileNodeFromBuffer({
-      buffer: response.data,
+      buffer: Buffer.from(arrBuffer),
       name: node.name,
       parentNodeId: node.id,
       createNode,
