ca0fddf7918e61616c578e2a348b4fd0bc6b80ff
Laurenz <laurenz.rasche@gmail.com>
2022-05-17 12:36:37 +0200

    Revert "use `node-fetch` instead of `axios`"
    This reverts commit 100dff71ab2e8af8ca75911df169cc17f6afd1d4.

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,7 +1,7 @@
 import { createFileNodeFromBuffer } from 'gatsby-source-filesystem';
+import axios from 'axios';
 import { staticMapRequest } from './src/staticmap';
-import fetch from 'node-fetch';
-import { Buffer } from 'buffer';
+
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 exports.onCreateWebpackConfig = ({ actions }) => {
@@ -32,12 +32,11 @@ exports.onCreateNode = async ({
   if (node.internal.type === 'GeometryJson') {
     const url = staticMapRequest(node, [800, 800]);
     // have to use createFileNodeFromBuffer due to url length limits in createRemoteFileNode :/
-    const arrBuffer = await fetch(url.toString()).then((response) =>
-      response.arrayBuffer()
-    );
-
+    const response = await axios.get(url.toString(), {
+      responseType: 'arraybuffer',
+    });
     createFileNodeFromBuffer({
-      buffer: Buffer.from(arrBuffer),
+      buffer: response.data,
       name: node.name,
       parentNodeId: node.id,
       createNode,
