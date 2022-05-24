62629d66a4be5df620e736bda0d7cc5515b53924
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-05-24 16:26:08 +0200

    add hover to attribution

--- src/components/Map/Attribution.tsx
+++ src/components/Map/Attribution.tsx
@@ -4,6 +4,7 @@ export const Attribution = () => {
   return (
     <div className="absolute bottom-0 right-0 z-10 translate-y-1 translate-x-1 pr-3 pb-1 text-xs">
       <a
+        className="hover:underline"
         href="https://www.maptiler.com/copyright/"
         target="_blank"
         rel="noreferrer"
@@ -11,13 +12,19 @@ export const Attribution = () => {
         &copy; MapTiler
       </a>{' '}
       <a
+        className="hover:underline"
         href="https://www.openstreetmap.org/copyright"
         target="_blank"
         rel="noreferrer"
       >
         &copy; OpenStreetMap contributors
       </a>{' '}
-      <a href="https://carto.com/" target="_blank" rel="noreferrer">
+      <a
+        className="hover:underline"
+        href="https://carto.com/"
+        target="_blank"
+        rel="noreferrer"
+      >
         &copy; CARTO
       </a>
     </div>
