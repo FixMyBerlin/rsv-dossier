073208e6d06023d4fe7991ca3d3d2131901fbe0f
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-06-09 17:24:25 +0200

    use our heading component

--- src/components/Steckbrief/RsvDetails.tsx
+++ src/components/Steckbrief/RsvDetails.tsx
@@ -4,6 +4,7 @@ import { RSVMap } from '~/components/Map';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { RsvProgressBar } from '~/components/Steckbrief';
 import { MetaJson, StaticMap } from '~/types/index';
+import { Heading2 } from '~/components/Text';
 
 type Props = {
   meta: MetaJson & StaticMap;
@@ -24,9 +25,7 @@ export const RSVDetails: React.FC<Props> = ({ meta, geometry }) => {
           <div className="mt-6">
             <RsvProgressBar current="planning" />
           </div>
-          <h2 className="mt-6 text-lg font-bold text-gray-900 sm:text-xl">
-            Kurzfassung
-          </h2>
+          <Heading2>Kurzfassung</Heading2>
           <p className="text-lg text-gray-500">{meta.general.description}</p>
           {meta.references.website && (
             <div className="mt-4 rounded-full">
@@ -38,15 +37,11 @@ export const RSVDetails: React.FC<Props> = ({ meta, geometry }) => {
               </ButtonLink>
             </div>
           )}
-          <h2 className="mt-6 text-lg font-bold text-gray-900 sm:text-xl">
-            Trassenführung
-          </h2>
+          <Heading2>Trassenführung</Heading2>
           <p className="text-lg text-gray-500 sm:text-xl">
             {`${meta.general.from} - ${meta.general.to}`}
           </p>
-          <h2 className="mt-6 text-lg font-bold text-gray-900 sm:text-xl">
-            Länge
-          </h2>
+          <Heading2>Länge</Heading2>
           <p className="text-lg text-gray-500 sm:text-xl">10&thinsp;km</p>
         </div>
       </div>
