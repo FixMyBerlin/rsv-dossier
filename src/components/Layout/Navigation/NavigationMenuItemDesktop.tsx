1e808333e7f499bb3a33a5e01847c30c72a6980d
Henri <henri@fixmycity.de>
2022-06-20 12:04:08 +0200

    Add underline hover animation
    Used https://tobiasahlin.com/blog/css-trick-animating-link-underlines/ as reference

--- src/components/Layout/Navigation/NavigationMenuItemDesktop.tsx
+++ src/components/Layout/Navigation/NavigationMenuItemDesktop.tsx
@@ -17,7 +17,7 @@ export const NavigationMenuItemDesktop: React.FC<Props> = ({
       className={classNames(
         { 'font-bold text-emerald-500': active },
         { ' text-slate-500  hover:text-slate-700': !active },
-        'text-md inline-flex items-center px-1 pt-1 font-medium'
+        'text-md relative inline-flex items-center px-1 pt-1 font-medium before:absolute before:bottom-0 before:left-0 before:block before:h-1 before:w-full before:origin-top-left before:scale-x-0 before:bg-emerald-500 before:transition-transform before:duration-300 before:ease-out before:content-[""] before:hover:scale-x-100'
       )}
     >
       {name}
