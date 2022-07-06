10cb6a0622545c094ceeb033039029bd6dcab640
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-07-06 13:05:58 +0200

    change display condition to: description !== ''

--- src/components/Steckbrief/RsvDetails.tsx
+++ src/components/Steckbrief/RsvDetails.tsx
@@ -26,22 +26,20 @@ export const RSVDetails: React.FC<Props> = ({ meta, geometry }) => {
             <RsvProgressBar current="planning" />
           </div>
           <Heading2>Kurzfassung</Heading2>
-          <p className="text-lg text-gray-500">
-            {meta.general.description}
-            {meta.general.source !== '' && (
-              <>
-                <br />
-                (Quelle:&nbsp;
-                <a
-                  href={meta.general.source}
-                  className="text-slate-600 hover:text-slate-700 hover:underline active:underline"
-                >
-                  {new URL(meta.general.source).host}
-                </a>
-                )
-              </>
-            )}
-          </p>
+          {meta.general.description !== '' && (
+            <p className="text-lg text-gray-500">
+              {meta.general.description}(
+              <br />
+              (Quelle:&nbsp;
+              <a
+                href={meta.general.source}
+                className="text-slate-600 hover:text-slate-700 hover:underline active:underline"
+              >
+                {new URL(meta.general.source).host}
+              </a>
+              ) )
+            </p>
+          )}
           {meta.references.website && (
             <ButtonLink newWindow to={meta.references.website} className="mt-6">
               Projektwebsite
