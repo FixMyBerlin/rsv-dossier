0d398e6e777c9a96ab25eb1697bc0d74497b2f4b
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-07-27 16:10:43 +0200

    raname field on geometries: `name` => `id`

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -19,7 +19,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
 exports.createSchemaCustomization = ({ actions: { createTypes } }) => {
   createTypes(`
     type MetaJson implements Node {
-      geoJson: GeometryJson! @link(from: "jsonId", by: "name")
+      geoJson: GeometryJson! @link(from: "jsonId", by: "jsonId")
       staticMap: File! @link(from: "jsonId", by: "name")
     }
   `);
@@ -34,12 +34,17 @@ exports.onCreateNode = async ({
 }) => {
   const jsonValidator = new Validator();
 
-  if (node.internal.type === 'MetaJson') {
-    const { id, jsonId, parent, children, internal, ...metaJson } = node;
+  const jsonSchema = {
+    MetaJson: metaJsonSchema,
+    GeometryJson: geometryJsonSchema,
+  };
+  const nodeType = node.internal.type;
+  if (nodeType === 'MetaJson' || nodeType === 'GeometryJson') {
+    const { id, jsonId, parent, children, internal, ...json } = node;
 
     // validate meta data against JSON schema
     try {
-      jsonValidator.validate({ id: jsonId, ...metaJson }, metaJsonSchema, {
+      jsonValidator.validate({ id: jsonId, ...json }, jsonSchema[nodeType], {
         throwError: true,
       });
     } catch (e) {
@@ -49,20 +54,7 @@ exports.onCreateNode = async ({
     }
   }
 
-  if (node.internal.type === 'GeometryJson') {
-    const { id, jsonId, parent, children, internal, ...geometryJson } = node;
-
-    // validate geometry against JSON schema
-    try {
-      jsonValidator.validate(geometryJson, geometryJsonSchema, {
-        throwError: true,
-      });
-    } catch (e) {
-      if (e instanceof ValidationError) {
-        console.error(e);
-      } else throw e;
-    }
-
+  if (nodeType === 'GeometryJson') {
     // generate static map from geometry
     const url = staticMapRequest(node, [1920, 1920]);
     // have to use createFileNodeFromBuffer due to url length limits in createRemoteFileNode :/
@@ -72,7 +64,7 @@ exports.onCreateNode = async ({
 
     createFileNodeFromBuffer({
       buffer: Buffer.from(arrBuffer),
-      name: node.name,
+      name: node.jsonId,
       parentNodeId: node.id,
       createNode,
       createNodeId,
--- src/pages/steckbriefe/{metaJson.jsonId}.tsx
+++ src/pages/steckbriefe/{metaJson.jsonId}.tsx
@@ -30,10 +30,10 @@ export default Radschnellweg;
 
 export const query = graphql`
   query ($jsonId: String!) {
-    geometry: geometryJson(name: { eq: $jsonId }) {
+    geometry: geometryJson(jsonId: { eq: $jsonId }) {
       type
       bbox
-      name
+      id: jsonId
       features {
         type
         bbox
