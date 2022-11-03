edc106a54288731b46325fd7b895d6032742cd4d
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-11-03 14:41:15 +0100

    add default option "Alle anzeigen"

--- src/components/Steckbrief/FederalStateList.tsx
+++ src/components/Steckbrief/FederalStateList.tsx
@@ -39,28 +39,26 @@ export const FederalStateList = () => {
     stateCount[state] = `${state} (${stateCount[state]})`;
   });
   return (
-    <div>
-      {/* <label
-    htmlFor="location"
-    className="block text-sm font-medium text-gray-700"
-  >
-    Location
-  </label> */}
+    <div className="w-52">
       <select
         id="location"
         name="location"
         className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
         defaultValue="Canada"
       >
-        {Object.keys(stateCount).map((state) => (
-          <option
-            onClick={() => {
-              window.location.href = `steckbriefe/${state.toLowerCase()}`;
-            }}
-          >
-            {stateCount[state]}
-          </option>
-        ))}
+        <option>Alle anzeigen</option>
+        {Object.keys(stateCount)
+          .sort()
+          .map((state) => (
+            <option
+              key={state}
+              onClick={() => {
+                window.location.href = `steckbriefe/${state.toLowerCase()}`;
+              }}
+            >
+              {stateCount[state]}
+            </option>
+          ))}
       </select>
     </div>
   );
