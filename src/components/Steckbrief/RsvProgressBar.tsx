daf3105078261cea4c4a097e1ccb9508ad08f739
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-11-11 15:30:17 +0100

    change proportions of progress bar

--- src/components/Steckbrief/RsvProgressBar.tsx
+++ src/components/Steckbrief/RsvProgressBar.tsx
@@ -31,16 +31,14 @@ export const RsvProgressBar: React.FC<Props> = ({ currentState }) => {
     'bg-yellow-500 border-yellow-500 text-white text-gray-600 pl-0.5';
   const unselectedStyle = 'text-gray-600 ml-0.5';
   return (
-    <div className="flex flex-row text-black">
+    <div className="flex flex-row pb-4 text-black">
       {progressStates.map((state) => (
         <div
           key={state}
           className={classNames(
-            'inline-flex items-center justify-center border-y-2 font-bold first:rounded-l-full first:border-l-2 last:rounded-r-full last:border-r-2 hover:cursor-default',
-            'py-[2vw] px-[2vw]',
-            'sm:py-[0.8rem] sm:px-[0.8rem]',
-            'lg:py-[1.1vw] lg:px-[1.1vw]',
-            'xl:py-[0.9rem] xl:px-[0.9rem]',
+            'inline-flex items-center justify-center border-y-2 font-bold hover:cursor-default',
+            'first:rounded-l-full first:border-l-2 last:rounded-r-full last:border-r-2',
+            'px-10 py-0.5',
             progressStates.indexOf(state) <=
               progressStates.indexOf(currentState)
               ? selectedStyle
