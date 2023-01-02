13db7dcb5d5a23b04cb10b80ae4813d91fe51cc4
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-01-02 15:37:28 +0100

    Disable zoom-on-scroll for small and medium screens

--- src/components/Map/DynamicMap.tsx
+++ src/components/Map/DynamicMap.tsx
@@ -7,10 +7,13 @@ import {
 } from '@turf/turf';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Map, { FullscreenControl } from 'react-map-gl';
 import { maptilerBaseUrl, maptilerKey } from '~/utils';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import resolveConfig from 'tailwindcss/resolveConfig';
 import { RSVPopup, RSVSegment } from '.';
+import tailwindConfig from '../../../tailwind.config';
 
 type BBox2d = [number, number, number, number];
 
@@ -43,6 +46,30 @@ export const DynamicMap: React.FC<
   //   setSelected(properties.id);
   // };
   // const [cursorStyle, setCursorStyle] = useState('grab');
+
+  // eslint-disable-next-line prefer-const
+  let [isScreenHorizontal, setIsScreenHorizontal] = useState(false);
+
+  useEffect(() => {
+    // https://tailwindcss.com/docs/configuration: Note that this will transitively pull in a lot of our build-time dependencies, resulting in bigger client-side bundle size. To avoid this, we recommend using a tool like babel-plugin-preval to generate a static version of your configuration at build-time.
+    // eslint-disable-next-line prefer-const
+    let lgMediaQuery = window.matchMedia(
+      `(min-width: ${resolveConfig(tailwindConfig).theme.screens.lg})`
+    );
+    // fullConfig.theme.screens.lg;
+    // => '1024px'
+
+    function onMediaQueryChange({ matches }) {
+      setIsScreenHorizontal(matches);
+    }
+
+    onMediaQueryChange(lgMediaQuery);
+    lgMediaQuery.addEventListener('change', onMediaQueryChange);
+
+    return () => {
+      lgMediaQuery.removeEventListener('change', onMediaQueryChange);
+    };
+  }, []);
   return (
     <Map
       initialViewState={{
@@ -55,6 +82,7 @@ export const DynamicMap: React.FC<
       mapStyle={`${maptilerBaseUrl}/style.json?key=${maptilerKey}`}
       maxBounds={bboxView as BBox2d}
       attributionControl={false}
+      scrollZoom={isScreenHorizontal}
       interactiveLayerIds={geometry.features.map(
         ({ properties }) => properties.id
       )}
