7aaff2ab274e18cc9cae0ca84a7217c59ed4fd2e
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-01-06 09:51:59 +0100

    Update style SteckbriefPage

--- src/components/SteckbriefPage/SteckbriefPage.tsx
+++ src/components/SteckbriefPage/SteckbriefPage.tsx
@@ -18,51 +18,60 @@ export const SteckbriefPage: React.FC<Queries.SteckbriefQuery> = ({
               `${meta.general.ref}: `}
             {meta.general.name}
           </h1>
-          <div className="mt-6">
+          <div className="mt-8">
             <SteckbriefPageProgressBar currentState={meta.state} />
           </div>
-          <Heading3>Kurzfassung</Heading3>
-          {meta.general.description && (
-            <p className="text-lg text-gray-500">
-              {meta.general.description}
-              <br />
-              (Quelle:&nbsp;
-              <a
-                href={meta.general.source}
-                className="text-slate-600 hover:text-slate-700 hover:underline active:underline"
-              >
-                {new URL(meta.general.source).host}
-              </a>
-              )
-            </p>
-          )}
+          <div className="mt-8">
+            <Heading3>Kurzfassung</Heading3>
+            {meta.general.description && (
+              <p className="text-lg text-gray-500">
+                {meta.general.description}
+                <br />
+                (Quelle:&nbsp;
+                <a
+                  href={meta.general.source}
+                  className="text-slate-600 hover:text-slate-700 hover:underline active:underline"
+                >
+                  {new URL(meta.general.source).host}
+                </a>
+                )
+              </p>
+            )}
+          </div>
           {meta.references.website && (
-            <ButtonLink newWindow to={meta.references.website} className="mt-6">
+            <ButtonLink
+              outline
+              newWindow
+              to={meta.references.website}
+              className="mt-6"
+            >
               Projektwebsite
             </ButtonLink>
           )}
-          <Heading2>Projektdetails</Heading2>
-          <Heading3>Trassenführung (von X nach Y)</Heading3>
-          <p className="text-lg text-gray-500 sm:text-xl">
-            {`${meta.general.from.city} - ${meta.general.to.city}`}
-          </p>
-          <Heading3>Länge</Heading3>
-          <p className="text-lg text-gray-500 sm:text-xl">
-            ca. {meta.general.length.toLocaleString('de-DE')}&thinsp;km
-          </p>
-          {Boolean(meta.stakeholders[0].name) && (
-            <>
-              <Heading3>Zuständigkeit</Heading3>
-              {meta.stakeholders.map((stakeholder) => (
-                <p className="text-lg text-gray-500 sm:text-xl">
-                  {stakeholder.name}
-                </p>
-              ))}
-            </>
-          )}
+          <div className="mt-8">
+            <Heading2>Projektdetails</Heading2>
+            <Heading3>Trassenführung (von X nach Y)</Heading3>
+            <p className="text-lg text-gray-500 sm:text-xl">
+              {`${meta.general.from.city} - ${meta.general.to.city}`}
+            </p>
+            <Heading3>Länge</Heading3>
+            <p className="text-lg text-gray-500 sm:text-xl">
+              ca. {meta.general.length.toLocaleString('de-DE')}&thinsp;km
+            </p>
+            {Boolean(meta.stakeholders[0].name) && (
+              <>
+                <Heading3>Zuständigkeit</Heading3>
+                {meta.stakeholders.map((stakeholder) => (
+                  <p className="text-lg text-gray-500 sm:text-xl">
+                    {stakeholder.name}
+                  </p>
+                ))}
+              </>
+            )}
 
-          <Heading3>Stand</Heading3>
-          <p className="text-lg text-gray-500 sm:text-xl">todo</p>
+            <Heading3>Stand</Heading3>
+            <p className="text-lg text-gray-500 sm:text-xl">todo</p>
+          </div>
         </div>
       </div>
       <div className="mx-auto flex aspect-square max-h-[860px] overflow-hidden overscroll-none md:max-w-[860px] lg:fixed lg:left-0 lg:top-[97.8px] lg:bottom-0 lg:z-10 lg:mx-0 lg:h-full lg:max-h-full lg:w-[48vw] lg:max-w-[48vw] lg:items-stretch">
