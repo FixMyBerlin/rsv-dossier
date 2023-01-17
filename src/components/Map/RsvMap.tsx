a5fbd6866a8f4f479a8f356815629abff68f6a29
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-01-17 14:59:38 +0100

    Add legend to RsvMap in Steckbrief

--- src/components/Map/RsvMap.tsx
+++ src/components/Map/RsvMap.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 import { getOptInCookie, OptIn } from '~/components/CookieConsent/';
 import { Attribution, DynamicMap } from '.';
 import { navHeightClasssName } from '../Layout';
+import { Legend } from './Legend';
 
 export const RSVMap: React.FC<Queries.SteckbriefQuery> = ({
   meta,
@@ -15,13 +16,18 @@ export const RSVMap: React.FC<Queries.SteckbriefQuery> = ({
   return (
     <div className="relative max-h-full max-w-full bg-[#F9FAFC]">
       {consent === null && (
-        <div className="absolute bottom-0 z-20 mx-2 translate-y-1 md:mx-5">
+        <div className="absolute bottom-16 z-20 mx-2 translate-y-1 md:mx-5">
           <OptIn setConsent={setConsent} />
         </div>
       )}
-      <div className="absolute bottom-0 right-0 z-10 mr-2 mb-1 translate-y-1 translate-x-1 text-xs">
-        <Attribution />
+
+      <div className="absolute bottom-0 right-0 left-0 z-10 ">
+        <div className="mr-2 mb-2 translate-y-1 translate-x-1 text-xs">
+          <Attribution />
+        </div>
+        <Legend />
       </div>
+
       <div className={classNames(navHeightClasssName, 'hidden lg:block')} />
       {consent && <DynamicMap geometry={geometry} />}
       <GatsbyImage
