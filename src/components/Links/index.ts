05fa70a5b5fd72ab9f5c8695571ba43e2f2abe56
Tobias <tobias@fixmycity.de>
2023-04-24 16:24:00 +0200

    Datenschutz: Update structure of doc

--- src/components/Links/index.ts
+++ src/components/Links/index.ts
@@ -4,4 +4,5 @@ export * from './ExternalLink';
 export * from './MailLink';
 export * from './MailToButtonLink';
 export * from './MailToLink';
+export * from './TelLink';
 export * from './TextLink';
