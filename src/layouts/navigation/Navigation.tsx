b62cb7493cbdf0b0d9ca73845ddf483b45e9683f
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2024-08-01 15:40:17 +0200

    update navigation

--- src/layouts/navigation/Navigation.tsx
+++ src/layouts/navigation/Navigation.tsx
@@ -1,30 +1,38 @@
 import { Disclosure, DisclosurePanel } from '@headlessui/react'
 import clsx from 'clsx'
 
+import { RSVLogo } from '@assets/tsx/RSVLogo'
 import { navigationLinks } from './navigationLinks.const'
 import { NavigationMenuItemDesktop } from './NavigationMenuItemDesktop'
 import { NavigationMenuItemMobile } from './NavigationMenuItemMobile'
 import { NavigationMobileMenuButton } from './NavigationMobileMenuButton'
 
-type Props = { path: string }
+type Props = { path: string; fixed?: boolean }
 
 export const navHeightClasssName = 'h-24'
-export const Navigation: React.FC<Props> = ({ path }) => {
+export const Navigation: React.FC<Props> = ({ path, fixed }) => {
   return (
     // https://tailwindui.com/components/application-ui/navigation/navbars
-    <Disclosure as="nav" className={'z-20 bg-gray-50 shadow-md shadow-gray-400'}>
+    <Disclosure
+      as="nav"
+      className={clsx(
+        fixed && 'fixed left-0 right-0 top-0',
+        'z-20 bg-gray-50 shadow-md shadow-gray-400',
+      )}
+    >
       {({ open }) => (
         <>
           <div className="px-12">
             <div className={clsx(navHeightClasssName, 'flex justify-between py-2')}>
               <nav className="flex w-full items-center justify-between">
                 <div className="w-48">
-                  {/* todo image / svg ?*/}
-                  <img
-                    className="h-24 w-48"
-                    src={'src/layouts/navigation/assets/RSVLogo.svg'}
-                    alt=""
-                  />
+                  {path === '/' ? (
+                    <RSVLogo />
+                  ) : (
+                    <a href="/">
+                      <RSVLogo />
+                    </a>
+                  )}
                 </div>
                 <div className="hidden md:flex md:space-x-10">
                   {navigationLinks.map((link) => (
--- src/layouts/navigation/navigationLinks.const.ts
+++ src/layouts/navigation/navigationLinks.const.ts
@@ -1,12 +1,11 @@
 export type navigationMenuItemProps = { name: string; to: string }
 
 export const navigationLinks: navigationMenuItemProps[] = [
-  { name: 'Start', to: '/' },
   { name: 'Steckbriefe', to: '/steckbriefe/' },
   { name: 'Kommunikation', to: '/kommunikation/' },
   { name: 'Planung', to: '/planung/' },
-  {
-    name: 'Feedback geben',
-    to: 'mailto:feedback@fixmycity.de?subject=Feedback Radschnellverbindungen',
-  },
+  // {
+  //   name: 'Feedback geben',
+  //   to: 'mailto:feedback@fixmycity.de?subject=Feedback Radschnellverbindungen',
+  // },
 ]
