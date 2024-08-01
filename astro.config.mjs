744e0425f96951ccaedb4f2b0eed53bcbc07b3ab
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2024-08-01 15:47:50 +0200

    install font and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
-import netlify from "@astrojs/netlify";
+import markdoc from '@astrojs/markdoc'
+import mdx from '@astrojs/mdx'
+import netlify from '@astrojs/netlify'
+import react from '@astrojs/react'
+import keystatic from '@keystatic/astro'
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic(), tailwind()],
+  integrations: [react(), markdoc(), mdx(), keystatic(), tailwind()],
   output: 'hybrid',
-  adapter: netlify()
-});
+  adapter: netlify(),
+})
